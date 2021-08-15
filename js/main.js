bc937a8fd27441e73845ae668878bc4117b40915
Nguyen The Dat <67305993+ndat2408@users.noreply.github.com>
2021-08-15 20:34:48 +0700

    final js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-// Send data to firebase database
+// This is my first website, so it's will have many bug & security problem, 
+// but it's very meaningful to me because it's will be save all my youth, so i
+// don't want it to be attacked by hacker, so i did block normal people to open DevTool 
+// Excuse me
+window.oncontextmenu = function () {
+    return false;
+};
+
+document.addEventListener("keydown", function(event){
+    var key = event.key || event.keyCode;
+
+    if (key == 123) {
+        return false;
+    } else if ((event.ctrlKey && event.shiftKey && key == 73) || (event.ctrlKey && event.shiftKey && key == 74)) {
+        return false;
+    }
+}, false);
+
+
+// Get data from user input
 var userName, relation, text, time;
 function Ready(){
-        userName = document.getElementById('peopleName').value;
-        relation = document.getElementById('relation').value;
-            text = document.getElementById('mainText').value;
-            time = new Date().toLocaleString();
+    userName = document.getElementById('peopleName').value;
+    relation = document.getElementById('relation').value;
+      review = document.getElementById('review').value;
+        text = document.getElementById('mainText').value;
 }
 
 // Click send button
@@ -17,10 +36,10 @@ document.getElementById('btn').onclick = function(){
             document.getElementById('btn').classList.remove("loading"); // remove wait cursor
         }, 500);
     } else{
-        firebase.database().ref('/users/' + userName).set({
-            ThoiGian:time,
+        firebase.database().ref('/users/'+relation+' '+userName).set({
             Ten:userName,
             QuanHe:relation,
+            NhanXet:review,
             LoiMuonNoi:text
         });
         setTimeout(function(){
@@ -28,7 +47,6 @@ document.getElementById('btn').onclick = function(){
             document.getElementById('btn').classList.remove("loading");
     }, 500);
     }
-
 }
 
 // Auto height textarea
@@ -42,10 +60,11 @@ textarea.addEventListener("keyup", e =>{
 
 const f_day = new Date('Aug 26, 2019 13:00:00').getTime();
 const printf = document.getElementById("xxx")
-setInterval(function(){ 
-    console.clear()
-    var   now = new Date().getTime();
-    var   diff =  now - f_day
+timer();
+var Reload = setInterval(timer, 900);
+function timer(){
+    var   now  = new Date();
+    var   diff =  now.getTime() - f_day
     var second = Math.floor(diff/(1000))
     
     var    year = Math.floor(second/31536000)
@@ -59,8 +78,13 @@ setInterval(function(){
     var  minute =  Math.floor(second/60)
         second -= (minute*60)
     
+    if (year < 10) year ='0'+ year
+    if (month < 10) month ='0'+ month
+    if (hour < 10) hour ='0'+ hour
+    if (day < 10) day ='0'+ day
+    if (minute < 10) minute ='0'+ minute
+    if (second < 10) second ='0'+ second
+
     var xxx=year+" năm "+month+" tháng "+day+" ngày "+hour+" giờ "+minute+" phút "+second+" giây"
     printf.innerHTML = xxx;
-    
-}, 990);
-
+}
