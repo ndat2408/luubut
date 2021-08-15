ac190e928ae4b9e68d3aad6470f79d5f6a9a93bf
Nguyen The Dat <67305993+ndat2408@users.noreply.github.com>
2021-08-15 20:33:53 +0700

    connect to firebase database

--- js/connect.js
+++ js/connect.js
@@ -1,5 +1,5 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+// Your web app's Firebase configuration
+var firebaseConfig = {
     apiKey: "AIzaSyDF7Bq8VoBID5FSzCw0IplP7a3qQCIKCgY",
     authDomain: "luubut-ndat2408.firebaseapp.com",
     projectId: "luubut-ndat2408",
@@ -7,5 +7,5 @@
     messagingSenderId: "647309093997",
     appId: "1:647309093997:web:47eb5a86b7d0db4c836364"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
